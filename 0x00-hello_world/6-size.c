f13829035abfe663dff330d44e73bb682e6e2c0d
Kehinde <Kennyakinbowale@gmail.com>
2023-02-16 14:58:32 -0800

    Sizeof datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
-* main - printts the sizeof various data types in C using the sizeof operator
+* main - prints the sizeof various data types in C using the sizeof operator in C
 * Return: 0 (Success)
 */
 int main(void)
