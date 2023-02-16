b9eaa1a002a0b6ead6c52b391931ead8bf705fa8
Kehinde <Kennyakinbowale@gmail.com>
2023-02-16 12:31:19 -0800

    Prints outputs using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(void){
+/*
+* main - Entry point
+* Return: Always 0;
+*/
+int main(void)/*prints programming is like building a multilingual puzzle*/
+{
 puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
