a086a2c4afd760bb65847d5dad642a81068a2375
Kehinde <Kennyakinbowale@gmail.com>
2023-02-17 07:56:40 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
 * main - prints the sizeof various data types in C using the sizeof C-operators
-* Return: 0 (Success)
+* Return: 0(Success)
 */
 int main(void)
 {
@@ -15,6 +15,5 @@ printf("Size of a int: %lu byte(s)\n", sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 printf("Size of a float: %lu byte(s)\n", sizeof(f));
-printf('\n');
 return (0);
 }
