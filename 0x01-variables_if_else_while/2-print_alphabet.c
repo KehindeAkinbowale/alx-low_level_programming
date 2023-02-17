2f7b094c5a591790e176b3a18739a494ac351564
Kehinde <Kennyakinbowale@gmail.com>
2023-02-17 03:42:14 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - Prints all the alphabet in lowercase using the while loop
+* Return: 0(Success)
+*/
+int main()
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return 0;
+}
