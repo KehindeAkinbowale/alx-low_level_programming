79dce271a0cce359ee2981154ff12002e2f43cb5
Kehinde <Kennyakinbowale@gmail.com>
2023-02-17 01:14:02 -0800

    Positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - using if statements to print numbers from the RAND function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n;
+int RAND_MAX;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("n is positive\n");
+}
+else if (n == 0)
+{
+printf("n is zero\n");
+}
+else if (n < 0)
+{
+printf("n is negative\n");
+}
+else
+{
+printf("n is an invalid number");
+}
+return (0);
+}
