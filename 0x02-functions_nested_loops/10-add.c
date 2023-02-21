fba49328064c57fc1201ff308fc320a874cc5434
Kehinde <Kennyakinbowale@gmail.com>
2023-02-21 15:30:31 -0800

    Prints the sum of two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include"main.h"
 /**
 * add(int, int)- prints the sum of two integers
+* @x: number to be inputed into the function
+* @y: number to be inputed into the function
 * Return: 0(Success)
 */
 int add(int x, int y)
