1c600bc44d78fe98c2d79f898d63b894a0a56155
Kehinde <Kennyakinbowale@gmail.com>
2023-02-21 05:55:38 -0800

    Alphabet ten times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,10 @@
+#include"main.h"
+/**
+* main - execute the print_alphabet_x10 function
+* Return: 0(Success)
+*/
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include"main.h"
 /**
 * print_alphabet_x10 - function that prints lowercase alphabets 10 times.
 * Return: 0 (Success)
@@ -11,17 +11,8 @@ for (i = 0; i < 11; i++)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-putchar(ch);
+_putchar(ch);
 }
-putchar('\n');
+_putchar('\n');
 }
-}
-/**
-* main - execute the print_alphabet_x10 function
-* Return: 0(Success)
-*/
-int main(void)
-{
-print_alphabet_x10(void);
-return (0);
 }
