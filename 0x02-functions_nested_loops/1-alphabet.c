7e2d48411f5aaa21ee68236501f48cb6539085fc
Kehinde <Kennyakinbowale@gmail.com>
2023-02-21 04:24:37 -0800

    Lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,20 +5,10 @@
 */
 void print_alphabet(void)
 {
+char ch;
 while (ch <= 'z')
 {
 putchar(ch);
 ch++;
 }
-}
-/**
-* main - print_alphabet function is executed in the main function
-* Return: 0 (Success)
-*/
-int main(void)
-{
-char ch = 'a';
-print_alphabet(void);
-putchar('\n');
-return (0);
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
-* main - check the code
-* Return: Always 0.
+* main - print_alphabet function is executed in the main function
+* Return: 0 (Success)
 */
 int main(void)
 {
-print_alphabet();
+char ch = 'a';
+print_alphabet(void);
+putchar('\n');
 return (0);
 }
