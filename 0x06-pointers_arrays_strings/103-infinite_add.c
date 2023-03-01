2f862bbfd6c282f556ade30a55e266d0c2e8379b
Kehinde <Kennyakinbowale@gmail.com>
2023-03-01 14:20:57 -0800

    Print buffer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,7 +7,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 * @n2: The string containing the second number to be added.
 * @r: The buffer to store the result.
 * @r_index: The current index of the buffer.
-*
 * Return: If r can store the sum - a pointer to the result.
 * If r cannot store the sum - 0.
 */
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ void print_buffer(char *b, int size);
 char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
