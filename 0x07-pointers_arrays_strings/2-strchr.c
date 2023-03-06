fd1043e8ca0ea40cd0e610cebc25e3860cc7d130
Kehinde <Kennyakinbowale@gmail.com>
2023-03-06 08:35:25 -0800

    character check in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,8 @@
 * _strchr - a function that locates a character in a string
 * @s: a string pointer
 * @c: a character to locate in string
-* Return: a string pointer or NULL if character not found
+* Return: If c is found - a pointer to the first occurence.
+* If c is not found - NULL
 */
 char *_strchr(char *s, char c)
 {
@@ -12,7 +13,7 @@ int i;
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == c)
-return ((s));
+return ((s + i));
 }
 return ('\0');
 }
