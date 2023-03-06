7d654d4f34f8c8b075035176ec67c1a8909c82cb
Kehinde <Kennyakinbowale@gmail.com>
2023-03-06 08:42:46 -0800

    character check in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,11 +9,11 @@
 */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
+int index;
+for (index = 0; s[index] >= '\0'; index++)
 {
-if (s[i] == c)
-return ((s + i));
+if (s[index] == c)
+return (s + index);
 }
 return ('\0');
 }
