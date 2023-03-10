1f73dc6cbd43108a8f3c92ec0fcc4a3ceb137c09
Kehinde <Kennyakinbowale@gmail.com>
2023-03-10 02:48:54 -0800

    prints the program name using command line arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include<stdio.h>
+/**
+* main - a function that prints its name using command line arguments
+* @argc: number of arguments in the function
+* @argv: a pointer pointing to an array of strings
+* Return: the program name
+*/
+int main(int argc, char *argv[])
+{
+int i = 0;
+if (argc == 1)
+{
+printf("%s\n", argv[i]);
+}
+return ((0));
+}
