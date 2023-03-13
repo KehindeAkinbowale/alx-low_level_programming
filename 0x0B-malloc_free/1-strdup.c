eee46b660e21bfaba54966f890558513e65a24c6
Kehinde <Kennyakinbowale@gmail.com>
2023-03-13 15:32:52 -0700

    Allocating memory for parameters

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include"main.h"
+#include<stdlib.h>
+#include<string.h>
+/**
+* _strdup - a function that returns a pointer which contains copy of string
+* @str: pointer to string characters
+* Return: a pointer to the duplicated string otherwise returns NULL
+*/
+char *_strdup(char *str)
+{
+int i;
+char *ptr_str;
+int n;
+n = strlen(str);
+ptr_str = (char *) malloc(n * (sizeof(char)));
+if (ptr_str != NULL)
+{
+for (i = 0; i < n; i++)
+{
+if (str == NULL)
+{
+return ((NULL));
+}
+else
+{
+ptr_str[i] = str[i];
+}
+}
+return ((ptr_str));
+}
+else
+{
+return((NULL));
+}
+}
