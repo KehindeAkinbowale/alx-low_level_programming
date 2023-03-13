f7d377549efc072ac50de7a14402451ddbe9a01c
Kehinde <Kennyakinbowale@gmail.com>
2023-03-13 14:38:54 -0700

    Array Memory allocation in C

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *ptr_c;
-ptr_c = (char *) malloc(size * (sizeof(char)));
+ptr_c = (char *)malloc(size * (sizeof(char)));
 if (size == 0)
 {
 return ((NULL));
