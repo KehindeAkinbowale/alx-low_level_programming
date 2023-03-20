234eb2a2dc7a89b04e1e9f2f1fe85154f4f79a8c
Kehinde <Kennyakinbowale@gmail.com>
2023-03-20 09:54:29 -0700

    Print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ d->name = ("nil");
 if (d->owner == NULL)
 d->owner = ("nil");
 printf("Name = %s\n", d->name);
-printf("Age = %.1f\n", d->age);
+printf("Age = %f\n", d->age);
 printf("Owner = %s\n", d->owner);
 }
