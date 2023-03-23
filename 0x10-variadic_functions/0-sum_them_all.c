95d45d20e5bc70adfeaac587401de9e2e34ca68a
Kehinde <Kennyakinbowale@gmail.com>
2023-03-23 14:08:00 -0700

    sum of variable arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,10 @@ int sum_them_all(const unsigned int n, ...)
 unsigned int i;
 unsigned int sum = 0;
 va_list arglist;
-if (n == 0)
-return (0);
+
 va_start(arglist, n);
 for (i = 0; i < n; i++)
-{
 sum += va_arg(arglist, unsigned int);
-}
 va_end(arglist);
 return (sum);
 }
