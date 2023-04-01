35c0158ff77d3ad9ac412626755308aba59e3b9c
Kehinde <Kennyakinbowale@gmail.com>
2023-03-31 19:23:34 -0700

    prints no of elements in list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,19 @@
 #include "lists.h"
 
 /**
-* list_len - returns the number of elements in a linked list
-* @h: pointer to the list_t list
+* list_len - returns the number of elements in list_t
+* @h: a pointer to the list_t
 *
-* Return: number of elements in h
+* Return: The number of elements in pointer h list_t
 */
 size_t list_len(const list_t *h)
 {
-size_t n = 0;
+size_t k = 0;
 
-while (h)
+while (h != NULL)
 {
-n++;
+k++;
 h = h->next;
 }
-return (n);
+return (k);
 }
