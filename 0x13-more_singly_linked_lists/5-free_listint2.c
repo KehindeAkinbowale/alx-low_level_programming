2809eba90f200b05f70d8c2aed7bd701f0ecb2b7
Kehinde <Kennyakinbowale@gmail.com>
2023-04-03 06:03:57 -0700

    free list second function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include"lists.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+* free_listint2 - a function that frees a listint_t list
+* @head: a pointer to the first node of the list
+* Return: nothing
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *ptr_temp;
+while ((*head) != NULL)
+{
+ptr_temp = (*head)->next;
+free(*head);
+(*head) = ptr_temp;
+}
+(*head) = NULL;
+}
