e4280ea006299661a37ac72fabb7e016c4280cb1
Kehinde <Kennyakinbowale@gmail.com>
2023-04-04 09:02:38 -0700

    print elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t Count = 0;
+size_t num_count = 0;
 
 while (h != NULL)
 {
 printf("%d\n", h->n);
-Count++;
+num_count++;
 h = h->next;
 }
-return (count);
+return (num_count);
 }
