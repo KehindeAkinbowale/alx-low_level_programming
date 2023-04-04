7b73e8a1a43174f9be6e000e5982fea5d94ef89a
Kehinde <Kennyakinbowale@gmail.com>
2023-04-04 09:07:17 -0700

    print elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t num_count = 0;
+size_t num = 0;
 
-while (h != NULL)
+while (h)
 {
 printf("%d\n", h->n);
-num_count++;
+num++;
 h = h->next;
 }
-return (num_count);
+
+return (num);
 }
