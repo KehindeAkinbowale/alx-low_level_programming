6d963dd4787c03cf1ab6e0fac2a96a5d9b559299
Kehinde <Kennyakinbowale@gmail.com>
2023-04-05 10:13:04 -0700

    Prints all elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t num_node;
-num_node = 0;
-while (h != NULL)
+size_t num_node = 0;
+while (h)
 {
 num_node++;
-printf("%d\n", h->n);
-
+printf("%d", h->n);
+printf("\n");
 h = h->next;
 }
+
 return (num_node);
 }
