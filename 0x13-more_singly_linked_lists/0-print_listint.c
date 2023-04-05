cecc7d76cab521c986ed1e4e7dc8a5bf017e23b5
Kehinde <Kennyakinbowale@gmail.com>
2023-04-05 10:01:46 -0700

    print all elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,10 +4,9 @@
 /**
 * print_listint - a function that prints all the data in the list
 * @h: a pointer to the head node of listint_t list
-*
 * Return: number of nodes
 */
-size_t listint_len(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 size_t num_node;
 num_node = 0;
@@ -15,8 +14,8 @@ while (h != NULL)
 {
 num_node++;
 printf("%d", h->n);
+printf("\n");
 h = h->next;
 }
-printf("\n");
 return (num_node);
 }
