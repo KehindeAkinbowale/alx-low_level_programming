e02a883803cbe6d995b36940cf2c524d189e177e
Kehinde <Kennyakinbowale@gmail.com>
2023-04-05 10:07:45 -0700

    Prints all elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,6 +4,7 @@
 /**
 * print_listint - a function that prints all the data in the list
 * @h: a pointer to the head node of listint_t list
+* Description: a function that prints the data inside the list
 * Return: number of nodes
 */
 size_t print_listint(const listint_t *h)
